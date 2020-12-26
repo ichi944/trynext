517a3907d2675b01dade64ad07ae1d6e6c2909bc
ichi944 <ichi944g@gmai..com>
2020-12-26 16:48:44 +0900

    fix

--- pages/api/new.ts
+++ pages/api/new.ts
@@ -4,6 +4,6 @@ const prisma = new PrismaClient()
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
 	console.log('@API.new: ', req);
-  const book = await prisma.book.create({ data: { ...req.body, genreId: 1 }});
+  const book = await prisma.book.create({ data: { ...req.body, genre: { connect: { id: 1 }} }});
 	res.status(200).json({ book });
 }
